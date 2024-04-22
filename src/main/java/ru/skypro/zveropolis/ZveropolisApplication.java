5c24f11da6bdd9c14d12b65d15a17e56274ed38a
Admin <samedi450511@gmail.com>
2024-04-23 00:42:38 +0600

    new

---
+++ src/main/java/ru/skypro/zveropolis/ZveropolisApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.zveropolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZveropolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZveropolisApplication.class, args);
+	}
+
+}
