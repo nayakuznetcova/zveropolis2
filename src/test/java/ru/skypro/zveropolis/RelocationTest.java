65476fa9e8876ec30260b716149c4ca4e2461d2a
MSI <kulakov056@mail.ru>
2024-05-08 23:03:37 +0500

    Удалил закоментированные тесты

--- src/test/java/ru/skypro/zveropolis/RelocationTest.java
+++ src/test/java/ru/skypro/zveropolis/RelocationTest.java
@@ -3,13 +3,28 @@ package ru.skypro.zveropolis;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.skypro.zveropolis.relocation.Relocation;
 import ru.skypro.zveropolis.relocation.State;
 import ru.skypro.zveropolis.repository.SubscriberRepository;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import ru.skypro.zveropolis.TelegramBotSendMessage;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.out;
+import static liquibase.repackaged.net.sf.jsqlparser.parser.feature.Feature.execute;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -19,40 +34,8 @@ public class RelocationTest {
     @Mock
     Relocation newRelocation;
 
-    @Test
-    void createNewRelocation() {
-
-        Relocation newRelocation = mock(Relocation.class);
-//        Relocation myRelocation = new Relocation();
-
-        when(newRelocation.getState(any(Long.class))).thenReturn((State) newRelocation);
-//        assertEquals(0, newRelocation.getState(1l));
-    }
 
     private RelocationTest relocationTest;
 
-    @BeforeEach
-    void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
 
-    @Mock
-    Relocation getNewRelocation;
-    @Mock
-    private SubscriberRepository subscriberRepository;
-
-//    @Test
-//    void getOwner(){
-//        Relocation newRelocation = Mockito.mock(Relocation.class);
-//        when(newRelocation.getOwner()).thenReturn("123");
-//        assertEquals("123", newRelocation.getOwner());
-//    }
-//
-//    @Test
-//    void verificationTest() {
-//        Relocation newRelocation = Mockito.mock(Relocation.class);
-//        verify(newRelocation);
-//        assertEquals(null, newRelocation.getOwner());
-//
-//    }
 }
